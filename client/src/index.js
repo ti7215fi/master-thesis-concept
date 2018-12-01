f8912cf6acbfeefbb6a26ab86dcfed749b2f6940
Tim Fischer <tim.fischer@fh-erfurt.de>
2018-12-01 20:17:36 +0100

    add form; save data to indexeddb

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,28 @@
-(function() {
-    window.localStorage.setItem('test1', '1');
-    window.localStorage.setItem('test2', '2');
-    window.localStorage.setItem('test3', '3');
-    window.localStorage.setItem('test4', '4');
-})();
+class User {
+    name;
+}
+
+/////////////////////////////
+
+const indexedDB = window.indexedDB;
+var db = null;
+
+const request = indexedDB.open('dbUsers', 1, (upgradeDb) => {
+    if (!upgradeDb.objectStoreNames.contains('users')) {
+        upgradeDb.createObjectStore('users', { keyPath: 'id', autoIncrement: true });
+    }
+});
+request.onerror = event => console.error('error');
+request.onsuccess = event => db = request.result;
+
+
+function add(data) {
+    let data = new User();
+    data.name = document.getElementById('addUserForm.fieldName');
+    const transaction = db.transaction("users", "readwrite")
+        .objectStore("users")
+        .add(data);
+
+    transaction.onsuccess = event => console.log('add success');
+    transaction.onerror = event => console.error('add error');
+}
